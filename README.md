cb5582f33395283451975ab4a528209c90c568f2
Tiago Jesus <tiagofilipe12@gmail.com>
2017-04-12 16:42:59 +0100

    added options for quering ncbi efetch and control to file outputs

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 Script used to retrieve .gb files from NCBI using eutils.
 
-Note: Given that I am having some problems with bad requests from NCBI, the script generates a list file (.txt) that can be used to run this script again using the '-l' option in order to retrieve the remaining sequences that were not retrieved in the first instance. Users are encoraged to check the output directory for the number of files using something like "ls | wc -l" (assuming that the directory has only .gb files). However, if an output .txt file contain entries, then you should run the script again in order to retrieve the remaining sequences. 
+Note: Given that I am having some problems with bad requests from NCBI, the script generates a list file (.txt) that can be used to run this script again using the '-l' option in order to retrieve the remaining sequences that were not retrieved in the first instance. Users are encoraged to check the output directory for the number of files using something like "ls | grep ".gb" | wc -l" (assuming that the directory has only .gb files). However, if an output .txt file contain entries, then you should run the script again in order to retrieve the remaining sequences. 
 
-**'-in'**,**'--input'**, dest='inputfile', help='Provide the input fasta files to parse')
+```
+**'-in'**,**'--input'**, help='Provide the input fasta files to parse'
 
-**'-l'**,**'--list'**, dest='listfile', help='instead of providing a fasta to parse, provide a txt file in which each row is a unique GI. One can parse the list provided of the GIs not found to try again... since NCBI rejects some connections.')
+**'-l'**,**'--list'**, help='instead of providing a fasta to parse, provide a txt file in which each row is a unique GI. One can parse the list provided of the GIs not found to try again... since NCBI rejects some connections.'
 
-**'-out'**,**'--output'**, dest='outputfile', required=True, help='Provide the output directory')
+**'-out'**,**'--output'**, help='Provide the output directory'
+
+**'-ft'**,**'--filetype'**, help='Provide file type. Options acc and gi.'
+
+**'-db'**,**'--dbtype'**, help='Provide database type. Options e.g. nuccore and protein.'
+
+**'-et'**,**'--exporttype'**, help='Provide export file type. Options e.g. fasta and gb.'
+```
--- gb_downloader.py
+++ gb_downloader.py
@@ -30,26 +30,29 @@ def downloader_from_list(list_file):
 		gi_list.append(line.strip("\n"))
 	return gi_list	
 
-def downloader(gi_list, output_path , tag):
+def downloader(gi_list, output_path , tag, file_type, db_type, export_type):
 	time_control = 1
 	counter = 0
 	for gi in gi_list:
-		if time_control%5 == 0:	#from 5 to 5 gi's the script pauses for 5 seconds in order to avoid ban from NCBI.
-			time.sleep(5) #time in seconds
-			time_control += 1
-			print "pausing..." + str(time_control-1) + " GIs already processed!"
-		else:
-			gi_file = gi + ".gb"
-			gbfile = urllib.URLopener()
-			gbfile.retrieve("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id="+gi+"&rettype=gb&retmode=text", os.path.join(output_path, gi_file))
-			time_control += 1
+		gi_file = gi + "." + export_type
+		gbfile = urllib.URLopener()
+		## if gi does not have an accession append a .1 to version
+		if file_type=="acc":
+			if "." not in gi:
+				gi+=".1"
+		url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db="+db_type+"&id="+gi+"&rettype="+export_type+"&retmode=text"
+		try:				
+			gbfile.retrieve(url, os.path.join(output_path, gi_file))
+		except IOError:
+			print "Error: url "+url+" does not exist"
+		time_control += 1
 	## Check output directory and retrieves a list of missing GI's
-	out_file_name=tag + ".txt"
-	if os.path.isfile(out_file_name):
-		shutil.move(out_file_name, out_file_name + ".backup")
-	output_checker = open(out_file_name, 'a')
-	for gi in gi_list:
-		if gi + ".gb" not in os.listdir(output_path):
+	
+	for x,gi in enumerate(gi_list):
+		if gi_file not in os.listdir(output_path):
+			if x==0:
+				output_checker = open(tag + "_missing.txt", 'w')
+
 			output_checker.write(gi + "\n")
 		else:
 			counter=+1
@@ -60,7 +63,15 @@ def main():
 	parser.add_argument('-in','--input', dest='inputfile', nargs='+', help='Provide the input fasta files to parse')
 	parser.add_argument('-l','--list', dest='listfile', help='instead of providing a fasta to parse, provide a txt file in which each row is a unique GI. One can parse the list provided of the GIs not found to try again... since NCBI rejects some connections.')
 	parser.add_argument('-out','--output', dest='outputfile', required=True, help='Provide the output directory')
+	parser.add_argument('-ft','--filetype', dest='filetype', required=True, help='Provide file type. Options acc and gi.')
+	parser.add_argument('-db','--dbtype', dest='dbtype', required=True, help='Provide database type. Options e.g. nuccore and protein.')
+	parser.add_argument('-et','--exporttype', dest='exporttype', required=True, help='Provide export file type. Options e.g. fasta and gb.')
+
 	args = parser.parse_args()
+	file_type = args.filetype
+	db_type = args.dbtype
+	export_type = args.exporttype
+
 	if args.inputfile:
 		list_fastas = args.inputfile
 		for infile in list_fastas:
@@ -79,7 +90,7 @@ def main():
 		output_path = os.path.join(os.path.dirname(os.path.abspath(args.listfile.strip())), args.outputfile)
 		if not os.path.exists(output_path):
 			os.makedirs(output_path)
-		downloader(downloader_from_list(args.listfile), output_path, args.listfile.split(".")[0])
+		downloader(downloader_from_list(args.listfile), output_path, args.listfile.split(".")[0],file_type, db_type, export_type)
 	else:
 		print "Did you provide any input file type? Or did you provided more than one type?"
 
